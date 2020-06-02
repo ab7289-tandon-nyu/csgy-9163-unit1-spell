ba1ae48ae600bc5f246978d37ef2a1f941ecceaf
Alex Biehl <32490273+ab7289@users.noreply.github.com>
2020-06-02 17:28:19 -0400

    create spell.c, spell.h, and test_spell.c per assignment

---
+++ tests/test_spell.c
@@ -0,0 +1,15 @@
+//
+//  test_main.c
+//  csgy-9163-unit1-spell
+//
+//  Created by Alex Biehl on 5/30/20.
+//  Copyright © 2020 Alex Biehl. All rights reserved.
+//
+#include <check.h>
+#include "spell.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+#define DICTIONARY "wordlist.txt"
+#define TESTDICT "test_wordlist.txt"
+
