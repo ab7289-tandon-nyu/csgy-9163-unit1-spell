fda815d50d1bdf3cf4eaf45d2a923f12dfab0ae8
Alex Biehl <ab7289@nyu.edu>
2020-06-07 10:23:42 -0400

    forgot to comment debugging line

--- tests/test_spell.c
+++ tests/test_spell.c
@@ -418,7 +418,7 @@ int main(void) {
     runner = srunner_create(suite);
     srunner_set_log(runner, "test.log");
     // uncomment the next line if debugging
-    // srunner_set_fork_status(runner, CK_NOFORK);
+    srunner_set_fork_status(runner, CK_NOFORK);
     srunner_run_all(runner, CK_VERBOSE);
     failed = srunner_ntests_failed(runner);
     srunner_free(runner);
